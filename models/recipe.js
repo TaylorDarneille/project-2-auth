a641af6e487a98ee0624be37b50d79e65a5e2c89
Loooai10 <honey_moonn@yahoo.com>
2022-10-24 10:08:28 +0300

    on progress

---
+++ models/recipe.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class recipe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  recipe.init({
+    title: DataTypes.STRING,
+    ingredients: DataTypes.STRING,
+    image: DataTypes.STRING,
+    diet: DataTypes.STRING,
+    allergies: DataTypes.STRING,
+    instruction: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'recipe',
+  });
+  return recipe;
+};
