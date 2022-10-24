356549c1ef04a6e2dd85d12eca91e91dfee1d1e6
Layla <layla.baqer97@gmail.com>
2022-10-24 16:58:29 +0300

    Added search bar

--- controllers/foods.js
+++ controllers/foods.js
@@ -7,23 +7,36 @@ router.get('/recipes', async (req, res)=>{
     res.render('foods/recipes.ejs')
 })
 
-router.get('/*', async (req, res)=>{
-    res.render('foods/index.ejs')
+router.get('/search', async (req, res)=>{
+    console.log(req.params)
+
+    axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${req.query.q}&app_id=10f33fbc&app_key=43f9be24513291624d49476ed4d0dd73`)
+    .then(apiResponse=>{
+        let foods = apiResponse.data.hits
+        console.log(foods)
+        // res.render('foods/recipes.ejs', {foods})
+        res.render('home.ejs', {foods})
+        // res.json(foods[0].recipe.label)
+    })
+    .catch(err => {
+        res.send(err)
+    })
 })
 
-router.get('/:id', async (req, res)=>{
+router.get('/:name', async (req, res)=>{
     console.log(req.params)
 
     axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${req.params.name}&app_id=10f33fbc&app_key=43f9be24513291624d49476ed4d0dd73`)
     .then(apiResponse=>{
-        let foods = apiResponse.data
-        console.log(foods)
-        // res.render('foods/recipes.ejs', {foods})
-        res.render('foods/index.ejs', {foods})
+        let food = apiResponse.data.hits[0]
+        console.log(food)
+        res.render('foods/index.ejs', {food})
+        // res.render('home.ejs', {foods: [foods]})
         // res.json(foods[0].recipe.label)
     })
-
-    res.render('foods/recipes.ejs')
+    .catch(err => {
+        res.send(err)
+    })
 })
 
 module.exports = router
